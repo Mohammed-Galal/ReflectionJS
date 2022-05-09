82a68296c3b52a4c6787f569b99e2e9d14b746e5
Mohammed-Galal <mohammedgalal682@gmail.com>
2022-05-09 02:06:38 +0200

    parser-rebuilt

--- XML_Parser/utils/index.js
+++ XML_Parser/utils/index.js
@@ -1,85 +1,154 @@
-const isScriptTag = (str) => /^{|}$/g.test(str),
-  isTxtContent = (str) => /^>/g.test(str),
-  isOpenTag = (str) => /<(?!\/)/g.test(str),
-  isEndTag = (str) => /^(\/|<\/)/g.test(str);
+const checkIfOpenScriptTag = (str) => /^{/.test(str),
+  checkIfEndScriptTag = (str) => /}$/.test(str),
+  checkIfOpenTag = (str) => /^<\w/.test(str),
+  checkIfEndTag = (str) => /<\/\w+>$|\/>$/.test(str),
+  checkIsEndTag = (str) => /^(\/|<\/\w)/.test(str);
 
-const translateFragmentsExp = /<>|<\/>/g,
-  contentSplitter = /(?=<[\w\/])|(?=[>{])|(?=\/>)|(?<=})/g;
-
-module.exports = function (txt) {
-  const afterFragmentsSet = String(txt).replace(
-    translateFragmentsExp,
-    (m) => (m === "<>" ? "<" : "</") + "Fragment>"
+const fillFragmentsExp = /<>|<\/>/g,
+  contentSplitter = /(?=<\w)|(?<=>)/g;
+module.exports = function (file) {
+  return handleTextContent(
+    String(file).replace(
+      fillFragmentsExp,
+      (m) => (m === "<>" ? "<" : "</") + "Fragment>"
+    )
   );
-  return handleContentChunks(afterFragmentsSet.split(contentSplitter));
 };
 
-let key = 0;
-function handleContentChunks(arr) {
-  const roots = [];
+function handleTextContent(text) {
+  const arr = text.split(contentSplitter),
+    endPos = arr.length;
 
-  // let isElOpen = false;
-  let currentContainerMethods = null;
-  const Content = arr.map((str) => {
-    if (isOpenTag(str)) {
-      if (currentContainerMethods === null) {
-        createRoot(handleOpenTags(str));
-        return roots.length - 1;
+  const roots = [],
+    contentWithoutRoots = [];
+
+  let strHolder = [];
+  countOfOpenEls = 0;
+
+  var currentIndex = 0;
+  while (currentIndex < endPos) {
+    const str = arr[currentIndex],
+      isOpenTag = checkIfOpenTag(str),
+      isEndTag = checkIfEndTag(str);
+    currentIndex++;
+
+    if (isOpenTag) countOfOpenEls++;
+
+    const targetArr = countOfOpenEls === 0 ? contentWithoutRoots : strHolder;
+    targetArr.push(str);
+
+    if (countOfOpenEls > 0 && isEndTag) {
+      countOfOpenEls--;
+      if (countOfOpenEls === 0) {
+        const Len = roots.length;
+        roots.push(strHolder.join(""));
+        strHolder = [];
+        contentWithoutRoots.push(Len);
       }
-      currentContainerMethods.appendEl(str);
     }
-    if (currentContainerMethods === null) return str;
-    else if (isEndTag(str)) {
-      currentContainerMethods.endEl(str[0] === "/");
-      if (currentContainerMethods === null) return roots[roots.length - 1];
-    } else if (isScriptTag(str)) currentContainerMethods.handleScripts(str);
-    currentContainerMethods.appendTxt(str);
-    return false;
-  });
-
-  function createRoot([tag, attrs]) {
-    const obj = {
-      scripts: [],
-      components: [],
-      dom: [[tag, attrs, []]],
-    };
-
-    roots.push(obj);
-
-    let scriptsHolder = [],
-      scriptsCounter = 0;
-
-    currentContainerMethods = {
-      appendEl(str) {
-        if (scriptsCounter > 0) return scriptsHolder.push(str);
-        const [tag, attrs] = handleOpenTags(str);
-        obj.dom.push([tag, attrs, []]);
-      },
-      appendTxt(txt) {
-        if (scriptsCounter > 0) return scriptsHolder.push(str);
-        obj.dom[obj.dom.length - 1][2].push(handleText(txt));
-      },
-      endEl(isSelfClosed) {
-        if (scriptsCounter > 0) return scriptsHolder.push(str);
-        const domLen = obj.dom.length,
-          targetEl = obj.dom[domLen - 2];
-        isSelfClosed && (targetEl[1].isSelfClosed = isSelfClosed);
-        if (domLen > 1) {
-          const childEl = obj.dom[domLen - 1];
-          targetEl[2].push(childEl);
-          return obj.dom.pop();
-        }
-        isElOpen = false;
-        currentContainerMethods = null;
-      },
-    };
   }
+
+  const parsedRoots = roots.map(parser);
 }
 
-function handleOpenTags(str) {
-  return [tag, attr];
-}
+const rootSplitExp = /(?=<\w)|(?=\/>)|(?=>)|(?=<\/\w)|(?={)|(?<=})/g;
+let key = 0;
+function parser(root) {
+  const arr = root.split(rootSplitExp),
+    endPos = arr.length;
 
-function handleText(str) {
-  return str[0] === ">" ? String(str).slice(1) : str;
+  const currentKey = key,
+    scripts = [],
+    components = [];
+  key++;
+
+  const DOMArray = [],
+    Contexts = {
+      active: [],
+      get current() {
+        return this.active[this.active.length - 1];
+      },
+      addContext(Context) {
+        this.active.push(Context);
+      },
+    };
+
+  let strHolder = [],
+    countOfOpenScript = 0;
+
+  var currentIndex = 0;
+  while (currentIndex < endPos) {
+    const str = arr[currentIndex],
+      isOpenScript = checkIfOpenScriptTag(str),
+      isEndScript = checkIfEndScriptTag(str),
+      isThereOpenScript = countOfOpenScript > 0;
+    currentIndex++;
+
+    if (isOpenScript) countOfOpenScript++;
+    else if (isThereOpenScript && isEndScript) {
+      countOfOpenScript--;
+
+      if (countOfOpenScript === 0) {
+        strHolder.push(str);
+        const result = strHolder.join("").slice(1, -1),
+          isScriptExist = scripts.indexOf(result);
+
+        if (isScriptExist !== -1) Contexts.current.appendText(isScriptExist);
+        else {
+          const scriptIndex = scripts.length;
+          scripts[scriptIndex] = result;
+          Contexts.current.appendText(scriptIndex);
+        }
+        continue;
+      }
+    }
+
+    if (isThereOpenScript) {
+      strHolder.push(str);
+      continue;
+    }
+
+    const isOpenTag = checkIfOpenTag(str),
+      isEndTag = checkIsEndTag(str);
+
+    if (isOpenTag) _createContext(str);
+    else if (isEndTag) Contexts.current.revoke();
+    else Contexts.current.appendText(str);
+  }
+
+  function _createContext(str) {
+    const [tag, attrs] = handleOpenTags(str),
+      children = [];
+
+    const el = [tag, attrs, children];
+    DOMArray.push(el);
+
+    Contexts.add({
+      revoke() {
+        const DomLen = DOMArray.length,
+          parent = DOMArray[DomLen - 2];
+        parent[2].push(el);
+        Contexts.active.pop();
+      },
+      appendText(txt) {
+        const result = txt[0] === ">" ? String(txt).slice(1) : txt;
+        children.push(result);
+      },
+    });
+  }
+
+  function handleOpenTags(str) {
+    const sliced = str.slice(1).split(/\s+(?=\S+=["'{])/),
+      tag = sliced[0],
+      attrsTxts = sliced.slice(1),
+      attrs = {};
+  }
+
+  return `{
+    "#isComponent":true,
+    _id: ${currentKey},
+    scripts: [${scripts}],
+    components: [${components}],
+    dom: ${DOMArray[0]}
+  }`;
 }
